c5dd479e503b0f1f77fc192df97ce02d1fb4f3e6
Sthembile <213526437@stu.ukzn.ac.za>
2015-03-30 15:06:52 +0200

    final counter

---
+++ Counter/Count.h
@@ -0,0 +1,24 @@
+#ifndef COUNT_H
+#define COUNT_H
+
+class Count
+{
+public:
+	Count();
+	void Counter();
+	void Counter(int,int,int,int);
+
+
+private:
+	int Start, Stop, Up, Down;
+
+};
+
+#endif
+
+
+
+
+
+
+
---
+++ Counter/Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<Windows.h>
+#include"Count.h"
+using namespace std;
+
+int main()
+{
+	Count TEST;
+	TEST.Counter();
+	
+	
+
+}
